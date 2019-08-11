d73d77b009c8c7b4ddc42babc9a9c025585a6d64
skyfee <78933934@qq.com>
2019-08-11 14:09:17 +0800

    支持预览

--- app/Admin/Controllers/BaseController.php
+++ app/Admin/Controllers/BaseController.php
@@ -16,6 +16,7 @@ class BaseController extends Controller
 //        parent::__construct($request);
 
         View::share('erp_api_domain', env('ERP_API_DOMAIN', ''));
+        View::share('shop_front_url', env('SHOP_FRONT_URL', ''));
         View::share('global_area', config('global_area'));
         View::share('money_sign', config('money_sign'));
 
