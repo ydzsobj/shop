4dbb00bd6accf6f526f76587be18169d9b8c8f04
skyfee <78933934@qq.com>
2019-10-08 16:11:44 +0800

    支持配置前台域名地址

--- app/Admin/Controllers/BaseController.php
+++ app/Admin/Controllers/BaseController.php
@@ -15,7 +15,7 @@ class BaseController extends Controller
     {
 
         View::share('erp_api_domain', env('ERP_API_DOMAIN', ''));
-        View::share('shop_front_url', env('SHOP_FRONT_URL', ''));
+        View::share('shop_front_url', env('SHOP_FRONT_URL', config('global_shop_domain')));
         View::share('global_area', config('global_area'));
         View::share('global_lang', config('global_lang'));
         View::share('money_sign', config('money_sign'));
