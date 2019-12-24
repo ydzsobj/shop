505a340d113172dd146b8758525d4c02d4664045
skyfee <78933934@qq.com>
2019-12-24 18:28:25 +0800

    视频限制大小调整

--- config/upload.php
+++ config/upload.php
@@ -7,13 +7,13 @@ return [
 
     'image_max' => 600,//KB
 
-    'video_max' => 2048,//KB
+    'video_max' => 2048*10,//KB
 
     'msg' => '文件 {name} (<b>{size} KB</b>) 超过允许最大上传大小 <b>{maxSize} KB</b>. 请重新上传',
 
     'image_tips' => '<span style="color: red;"> 单图大小不超过600K </span>',
 
-    'video_tips' => '<span style="color: red;"> 视频大小不超过2M,支持MP4,AVI格式 </span>',
+    'video_tips' => '<span style="color: red;"> 视频大小不超过10M,支持MP4,AVI格式 </span>',
 ];
 
 
